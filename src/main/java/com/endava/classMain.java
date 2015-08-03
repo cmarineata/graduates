25cc92687b66494cc6d5e0cb19fe3373a511604b
cmarineata <costina.marineata@endava.com>
2015-08-03 18:19:41 +0300

    update tema

---
+++ src/main/java/com/endava/classMain.java
@@ -0,0 +1,10 @@
+package com.endava;
+
+/**
+ * Created by cmarineata on 8/3/2015.
+ */
+public class classMain {
+    public static void main(String [] args){
+        System.out.println("Hello,Costina!");
+    }
+}
